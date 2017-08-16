bd3d32856ec561070b5744e7eff73065b6255eb9
rosso357 <hiroshi.igari@rakus.co.jp>
2017-08-16 16:34:13 +0900

    (1-2)初級 入力値エラー処理を実装しました。

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberForm.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberForm.java
@@ -1,32 +1,46 @@
 package jp.co.rakus.stockmanagement.web;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
  * メンバー関連のリクエストパラメータが入るフォーム.
+ * 
  * @author igamasayuki
  *
  */
 public class MemberForm {
 	/** 名前 */
+	@NotBlank(message = "名前は必須です")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message = "メールアドレスの形式が違います。")
 	private String mailAddress;
 	/** パスワード */
+	@Size(min = 1, max = 8, message = "１文字以上８文字未満で入力してください。")
 	private String password;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailAddress() {
 		return mailAddress;
 	}
+
 	public void setMailAddress(String mailAddress) {
 		this.mailAddress = mailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
